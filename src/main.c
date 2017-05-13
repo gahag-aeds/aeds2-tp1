564b055c10b50f3ec01d4525dfeefaf379ab9add
gahag <gahag@hotmail.com.br>
2017-05-13 18:56:44 -0300

    Updated libaeds.

--- src/main.c
+++ src/main.c
@@ -55,27 +55,27 @@ int main(int argc, char *argv[]) {
   array_fill(cfg.cashiers_count, (void**) cashiers, NULL);
   
   queue user_cashier_queues[cfg.cashiers_count]; // One queue for each cashier.
-  foreach (i, 0, cfg.cashiers_count)
+  foreach_ix (i, 0, cfg.cashiers_count)
     user_cashier_queues[i] = new_lqueue(llist_node_pool);
   
   queue user_tray_queues[cfg.tray_stacks_count]; // One queue for each tray stack.
-  foreach (i, 0, cfg.tray_stacks_count)
+  foreach_ix (i, 0, cfg.tray_stacks_count)
     user_tray_queues[i] = new_lqueue(llist_node_pool);
   
   stack tray_stacks[cfg.tray_stacks_count];
-  foreach (i, 0, cfg.tray_stacks_count)
+  foreach_ix (i, 0, cfg.tray_stacks_count)
     tray_stacks[i] = new_vstack(mallocator, cfg.tray_stack_max);
   
   foodservice food_services[cfg.tray_stacks_count]; // One food service for each tray stack.
-  foreach (i, 0, cfg.tray_stacks_count)
+  foreach_ix (i, 0, cfg.tray_stacks_count)
     food_services[i] = new_foodservice(mallocator, cfg.food_service_size);
   
   
   for (time elapsed_time = 0; elapsed_time < total_time; elapsed_time++) {
     // First of all, reload the tray stacks:
     if (elapsed_time % cfg.tray_reload_rate == 0)
-      foreach (i, 0, cfg.tray_stacks_count) // In each tray stack
-        foreach (_, 0, cfg.tray_reload_load) { // Reload the specified number of trays.
+      foreach_ix (i, 0, cfg.tray_stacks_count) // In each tray stack
+        foreach_ix (_, 0, cfg.tray_reload_load) { // Reload the specified number of trays.
           if (stack_push(&tray_stacks[i], phantom_tray))
             trays_count++;
           else
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
     
     user* usr = NULL; // Intermediary variable.
     
-    foreach (i, 0, cfg.user_income) {
+    foreach_ix (i, 0, cfg.user_income) {
       // User arrives the course:
       usr = al_alloc(user_pool, 1, sizeof(user));
       *usr = (user) {
@@ -97,7 +97,7 @@ int main(int argc, char *argv[]) {
     }
     
     
-    foreach (i, 0, cfg.cashiers_count) {
+    foreach_ix (i, 0, cfg.cashiers_count) {
       // User leaves the cashier, proceeding to the tray queue:
       usr = cashiers[i];
       
@@ -109,7 +109,7 @@ int main(int argc, char *argv[]) {
     }
     
     
-    foreach (i, 0, cfg.tray_stacks_count) {
+    foreach_ix (i, 0, cfg.tray_stacks_count) {
       if (!stack_empty(tray_stacks[i])) {
         // User leaves the tray queue, proceeding to the respective tray stack:
         usr = dequeue(&user_tray_queues[i]);
@@ -149,11 +149,11 @@ int main(int argc, char *argv[]) {
   printf("Average user time: %zuh %zum\n", average_user_time / 60, average_user_time % 60);
   
   
-  foreach (i, 0, cfg.cashiers_count)
+  foreach_ix (i, 0, cfg.cashiers_count)
     // Delete user_cashier_queues:
     delete_queue(&user_cashier_queues[i], NULL, null_allocator());
   
-  foreach (i, 0, cfg.tray_stacks_count) {
+  foreach_ix (i, 0, cfg.tray_stacks_count) {
     // Delete user_tray_queues:
     delete_queue(&user_tray_queues[i], NULL, null_allocator());
     
